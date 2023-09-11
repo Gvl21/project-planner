584272802b93ecfd43521d8dcef26b82bef3c4e5
Gvl21 <thornyk1@gmail.com>
2023-09-11 16:37:21 +0900

    api 테스트용 맵 추가

--- planner/src/App.js
+++ planner/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Map from './components/Map';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { kakao } = window;
+    var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+        mapOption = {
+            center: new kakao.maps.LatLng(37.56682, 126.97865), // 지도의 중심좌표
+            level: 3, // 지도의 확대 레벨
+            mapTypeId: kakao.maps.MapTypeId.ROADMAP, // 지도종류
+        };
+
+    // 지도를 생성한다
+    var map = new kakao.maps.Map(mapContainer, mapOption);
+
+    return <div className='App'>{/* <Map /> */}</div>;
 }
 
 export default App;
---
+++ planner/src/components/Map.jsx
@@ -0,0 +1,10 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useEffect, useState } from 'react';
+import './Map.css';
+import axios from 'axios';
+
+function Map() {
+    return;
+}
+
+export default Map;
