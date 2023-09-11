dd97142f13294390877899d74b2aea7d13b15455
Gvl21 <thornyk1@gmail.com>
2023-09-11 20:03:40 +0900

    리액트로 지도 구현 완료

--- planner/src/App.js
+++ planner/src/App.js
@@ -2,18 +2,12 @@ import './App.css';
 import Map from './components/Map';
 
 function App() {
-    const { kakao } = window;
-    var mapContainer = document.getElementById('map'), // 지도를 표시할 div
-        mapOption = {
-            center: new kakao.maps.LatLng(37.56682, 126.97865), // 지도의 중심좌표
-            level: 3, // 지도의 확대 레벨
-            mapTypeId: kakao.maps.MapTypeId.ROADMAP, // 지도종류
-        };
-
-    // 지도를 생성한다
-    var map = new kakao.maps.Map(mapContainer, mapOption);
-
-    return <div className='App'>{/* <Map /> */}</div>;
+    return (
+        <div className='App'>
+            {' '}
+            <Map />{' '}
+        </div>
+    );
 }
 
 export default App;
--- planner/src/components/Map.jsx
+++ planner/src/components/Map.jsx
@@ -1,10 +1,23 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import React, { useEffect, useState } from 'react';
 import './Map.css';
-import axios from 'axios';
+
+const { kakao } = window;
 
 function Map() {
-    return;
+    useEffect(() => {
+        const container = document.getElementById('map');
+        const options = {
+            center: new kakao.maps.LatLng(35.1531954, 129.1189465),
+            level: 4,
+        };
+        const map = new kakao.maps.Map(container, options);
+    }, []);
+
+    return (
+        <div id='map'>
+            <div></div>
+        </div>
+    );
 }
 
 export default Map;
