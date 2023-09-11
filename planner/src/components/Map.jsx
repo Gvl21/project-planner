8c3adb1f93c2ba516a76f752710abbe9920da594
Gvl21 <thornyk1@gmail.com>
2023-09-11 23:27:57 +0900

    시작시 현재 좌표를 중심으로 지도가 시작되도록 바꿈

--- planner/src/components/Map.jsx
+++ planner/src/components/Map.jsx
@@ -2,15 +2,29 @@ import React, { useEffect, useState } from 'react';
 import './Map.css';
 
 const { kakao } = window;
-
 function Map() {
-    useEffect(() => {
+    const [posi, setPosi] = useState([0.0, 0.0]);
+
+    const geoOk = (position) => {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+        setPosi([lat, lng]);
+
+        // 위치 정보를 받아온 후에 지도 생성
         const container = document.getElementById('map');
         const options = {
-            center: new kakao.maps.LatLng(35.1531954, 129.1189465),
+            center: new kakao.maps.LatLng(lat, lng),
             level: 4,
         };
         const map = new kakao.maps.Map(container, options);
+    };
+
+    const geoError = () => {
+        console.log("didn't work :(");
+    };
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(geoOk, geoError);
     }, []);
 
     return (
